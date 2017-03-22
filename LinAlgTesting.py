a54f812db85c9cb7e6f9158477e71cdf29127910
William Derksen <william.derksen@students.olin.edu>
2017-03-22 18:32:06 -0400

    Finished Basic Testing

--- LinAlgTesting.py
+++ LinAlgTesting.py
@@ -1,5 +1,8 @@
 import numpy as np
 import scipy.io
+import scipy.misc
+import matplotlib.pyplot as plt
+from math import sqrt
 
 a = np.array([[1,2,3],[4,5,6]])
 b = np.array([[1, -1], [1, 1]])
@@ -16,4 +19,24 @@ face_data = mat['faces_train']
 sh = face_data.shape
 print(sh)
 
-np.reshape(face_data,(sh[0]*sh[1], sh[2]))
+f_re = np.reshape(face_data,(sh[0]*sh[1], sh[2]))
+f_mean  = np.expand_dims(np.mean(f_re, axis=0), axis=0)
+print(f_mean.shape)
+f_mean_centered = f_re - (np.ones((sh[0]*sh[1],1)) @ f_mean)/sqrt(sh[0]*sh[1])
+print(f_mean_centered)
+f_cov = (np.transpose(f_mean_centered) @ f_mean_centered) / (sh[0]*sh[1])
+[v, d] = np.linalg.eig(f_cov)
+print(f_cov)
+print(v)
+print(d)
+
+
+
+"""f_ave_data = f_re @ (np.ones((sh[2],1)))/sh[2]
+f_ave = np.reshape(f_ave_data,(sh[0], sh[1]))
+scipy.misc.imsave('outfile.png', f_ave)
+
+## Test Eig and Covarience
+#[v, d] = np.linalg.eig(f_re)
+print(v)
+print(d)"""
