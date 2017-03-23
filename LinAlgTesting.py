4b073202504bc0ebb6624710fac79ddddc2bcb4a
Adam Selker <aselker@openmailbox.org>
2017-03-23 09:46:39 -0400

    Cleaned up code and output, prepared for better testing and named guess

--- LinAlgTesting.py
+++ LinAlgTesting.py
@@ -4,64 +4,42 @@ import scipy.misc
 import matplotlib.pyplot as plt
 from math import sqrt
 
-
-
-"""a = np.array([[1,2,3],[4,5,6]])
-b = np.array([[1, -1], [1, 1]])
-
-print(a)
-print(b)
-
-c = b @ a
-print(c)"""
-input_face_index = 200
 basis_size = 60
+
+print("Loading and preparing training  data...")
 
 mat = scipy.io.loadmat('face_detect.mat')
 
-#type(mat)
-face_data = mat['faces_train']
-sh = face_data.shape
-#print(sh)
+faces_train = mat['faces_train']
+names_train = mat['names_train']
+faces_test_easy = mat['faces_test_easy']
+names_test_easy = mat['names_test_easy']
+faces_test_hard = mat['faces_test_hard']
+names_test_hard = mat['names_test_hard']
+sh = faces_train.shape
 
-f_re = np.reshape(face_data,(sh[0]*sh[1], sh[2]))
+f_re = np.reshape(faces_train,(sh[0]*sh[1], sh[2]))
 f_mean  = np.expand_dims(np.mean(f_re, axis=0), axis=0)
 #print(f_mean.shape)
-print((np.ones((sh[0]*sh[1],1)) @ f_mean).shape)
+#print((np.ones((sh[0]*sh[1],1)) @ f_mean).shape)
 f_mean_centered = (f_re - (np.ones((sh[0]*sh[1],1)) @ f_mean))
 
-#print(f_mean_centered)
+print('Calculating eigensystem...')
+
 f_cov = (f_mean_centered.T @ f_mean_centered) / (sh[1]*sh[0])
 [lambs, evecs] = np.linalg.eig(f_cov)
 [lambs, evecs] = np.real(lambs), np.real(evecs)
-print('evecs*evecs.T presorting: ')
-print(evecs @ evecs.T)
-
-###############################################################
 
 idx = lambs.argsort()[::-1]
 eigenValues = lambs[idx]
 evecs = evecs[:,idx]
-#to_be_sorted = np.hstack((np.expand_dims(lambs,1), evecs))
 
-print('evecs*evecs.T sorting:')
-print(evecs @ evecs.T)
+print('Making face basis with ' + str(basis_size) + ' eigenvectors...')
 
-
-#print(lambs.shape)
-#print(evecs.shape)
 evecs2 = f_mean_centered @ evecs
 face_basis = evecs2 / np.linalg.norm(evecs2, axis=0)
 
-#eig_face_1 = np.reshape(np.real(face_basis[:,1]), (sh[0], sh[1]))
-#scipy.misc.imsave('eigenFace1.png', eig_face_1)
 faces_proj = f_re.T @ face_basis
-print('faces reshape data:')
-print(f_re.shape)
-print('faces basis: ')
-print(face_basis.shape)
-print('faces projection:')
-print(faces_proj.shape)
 
 faces_predictor = faces_proj[:,:basis_size]
 
@@ -69,20 +47,32 @@ faces_predictor = faces_proj[:,:basis_size]
 #face_1 = np.reshape(face_data_1, (sh[0], sh[1]))
 #scipy.misc.imsave('FaceTest1.png', face_1)
 
-print(face_basis.T @ face_basis)
+print('Done creating basis, loading test image...')
 
-input_face = np.expand_dims(f_re[:,input_face_index], axis=0)
+##################################################################################
+
+
+
+input_face_index = 5
+
+test_faces = faces_test_easy
+test_names = names_test_easy
+
+tsh = test_faces.shape
+test_faces = np.reshape(test_faces, (tsh[0]*tsh[1], tsh[2]))
+
+input_face = np.expand_dims(test_faces[:,input_face_index], axis=0)
+
+print('Calculating test eigensystem...')
 
 input_face_eig = input_face @ face_basis[:,:basis_size]
 
-print("Input face eigenvector weights:")
-print(input_face_eig)
+print('Classifying face...')
 
 eigen_weight_diffs = (np.ones((sh[2],1)) @ input_face_eig) - faces_predictor
 
 dists = np.linalg.norm(eigen_weight_diffs, axis=1)
-print(dists.shape)
 face_guess_ind = np.argmin(dists)
-print(face_guess_indX)
 
-#for i in range(len(faces_predictor)):
+print('Face guess: ' + str(face_guess_ind))
+
